65a9c4d488319cb693ed858178d5b3e6a49d730d
Duk Young Kim <kdy_1406@hanmail.net>
2018-04-06 14:22:16 +0900

    adding project

---
+++ app/src/main/java/com/example/a13/myapplication/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.a13.myapplication;
+
+import android.os.Bundle;
+
+/**
+ * Created by 13 디콘 김덕영 on 2018-04-06.
+ */
+
+public class LoginActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a13/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a13.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void addClick(View v){
+        EditText login = (EditText) findViewById(R.id.login);
+    }
+}
