9bfac480c5a3dab2eb411d1b1b21517f6df69544
Duk Young Kim <kdy_1406@hanmail.net>
2018-04-14 21:15:39 +0900

    Setting User Information (not complete) App icon change

--- callivoice_app/src/main/java/com/example/callivoice/SettingActivity.java
+++ callivoice_app/src/main/java/com/example/callivoice/SettingActivity.java
@@ -3,7 +3,9 @@ package com.example.callivoice;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -45,5 +47,13 @@ public class SettingActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),"뒤로가기", Toast.LENGTH_LONG).show();
         finish();
     }
+
+
+    public void onClickUserInfo(View v){ //user 정보 열기 창
+        Intent intent = new Intent(getApplicationContext(),SettinginfoActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
---
+++ callivoice_app/src/main/java/com/example/callivoice/SettinginfoActivity.java
@@ -0,0 +1,25 @@
+package com.example.callivoice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SettinginfoActivity extends Activity{//회원 정보 받아오기 (미완성 : 정보 받아온거 입력하기)
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_settinguserinfo);
+
+    }
+
+
+
+}
